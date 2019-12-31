635b3c5dca651c99929c890a8bec50a0ed893148
nouriach <49981579+nouriach@users.noreply.github.com>
2019-12-31 15:50:40 +0000

    New Branch Effort
    This uses just the one API call and tries to work through the string to display a modified, simpler version of the game. Comments have been included. Need to work out how to extract the clubs for each player.

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -9,11 +9,9 @@ class FetchData extends React.Component {
         super(props);
         this.state = {
             playerPool: [],
-            playerName: '',
-            playerData: [],
+            playerName: [],
             playerClubs: [],
             playerYears: [],
-            playerGoals: [],
             playerGames: [],
             isLoaded: false,
         };
@@ -31,107 +29,70 @@ class FetchData extends React.Component {
                 this.setState ({
                     playerPool: data,
                 })
-            this.updatePlayer(this.state.playerPool);
+            this.removeDataGaps(this.state.playerPool);
         })
         .catch(err => console.error(err)
         )
     }
 
-    updatePlayer = (d) => {
-        let playerFname;
-        let playerLname;
-        let nameIdArray = [];
-        let split = d.toString().split('|');
+    removeDataGaps = (d) => {
+        let allData = d.toString().split('|');
+        let playerNameArray = [];
+        let playerClubsArray = [];
+        let playerYearsArray = [];
+        let playerGamesArray = [];
+
         let i;
-        for (i = 0; i < split.length; i++) {
-            if (split[i].includes('sortname')) {
-                nameIdArray.push(i);
+        for (i=0; i < allData.length; i++) {
+            if (allData[i] === "" || allData[i].includes('sortname')) {
+                // remove empty spaces
+                allData.splice(i,1);
+                i--;
+
             }
-            else {
+            else if (allData[i].includes('flagicon')) {
+                // set player appearances
+                let playerGames = allData[i+5]
+                playerGamesArray.push(playerGames);
+            }
+            else if (allData[i].includes('align="left"')){
+                // set player name
+                let nameLoop = `${allData[i+2]} ${allData[i+3]}`;
+                // set player career years
+                let yearsLoop = `${allData[i-3]} - ${allData[i-2]}`
+                playerNameArray.push(nameLoop);
+                playerYearsArray.push(yearsLoop);
             }
         }
-        console.log('outside of loop');
-        let randId = nameIdArray[Math.floor(Math.random()*nameIdArray.length)];
+        /**** 
+          Below each state array has the same amount of values in it, therefore
+          number '30' will always represent the same player's information.
+          PlayerClubs is still incomplete.
+         ****/ 
+        this.setState ({
+            playerName: playerNameArray,
+            playerClubs: [],
+            playerYears: playerYearsArray,
+            playerGames: playerGamesArray,
+        })
 
-        playerFname = split[randId + 1].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');;
-        playerLname = split[randId + 2].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-       this.fetchData(playerFname, playerLname);
+        this.buildClubs(allData);
     }
 
-    fetchData = (a, b) => {
-        let link = `https://en.wikipedia.org/w/api.php?&origin=*&action=parse&page=${a}%20${b}&format=json&prop=wikitext&section=0`;
-        fetch(link)
-        .then(response => response.json())
-        .then(data => {
-            console.log(link);
-            if (!data.parse.wikitext['*'].includes('clubs1')) {
-                this.fetchPlayerData();            
-                }
-            else {
 
-                this.setState ({
-                    isLoaded: true,
-                    playerName: data.parse.title,
-                    playerData: data.parse.wikitext['*'],
-                    playerClubs: [],
-                    playerYears: [],
-                    playerGoals: [],
-                    playerGames: [],
-                    })
-                }
-            })
-        .catch(err => console.error(err)
-        )
+    /**** NEXT STEP ****/
+    // How do I join the string together again like below and then extract just the clubs?
+
+    buildClubs = (e) => {
+        let joinData = e.join()
+        console.log(joinData);
     }
+
 
     render() {
         let { isLoaded, playerName, playerData, playerClubs, playerYears, playerGoals, playerGames } = this.state;
 
-        let sliceStrStart = playerData.indexOf("years1 ");
-        let sliceStrEnd = playerData.indexOf("totalcaps");
-        this.state.playerClubs = playerData.slice(sliceStrStart, sliceStrEnd)
-        let split = this.state.playerClubs.toString().split(' ');
-        let clubsArray = [];
-        let yearsArray = [];
-        let goalsArray = [];
-        let gamesArray = [];
 
-        let i;
-        for (i = 0; i < split.length; i++) {
-            if  (split[i] == "") {
-                split.splice(i,1);
-                i--;
-            }
-            else if (split[i].includes('clubs') & !split[i].includes('youth'))  {
-                let club = split[i + 2].replace(/[^a-z^A-Z^0-9→\^]/gi, '');
-                let clubTwo = split[i + 3].replace(/[^a-z^A-Z^0-9→\^]/gi, '');
-                if(clubTwo.includes(club)) {
-                    clubTwo = clubTwo.slice(0, (clubTwo.length - club.length));
-                }
-                let finalClub = club + ' ' + clubTwo;
-                clubsArray.push(finalClub)
-                this.state.playerClubs = clubsArray;
-            }
-        };
-
-        let e;
-        for (e = 1; e < split.length; e++) {
-            if (split[e].includes('year') & !split[e].includes('youth') & !split[e].includes('national')){
-                let yearsResult = split[e+2].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');;
-                yearsArray.push(yearsResult);
-                this.state.playerYears = yearsArray;
-            } 
-            else if (split[e].includes('goals') & !split[e].includes('youth') & !split[e].includes('national')) {
-                let goals = split[e+2].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-                goalsArray.push(goals);
-                this.state.playerGoals = goalsArray;
-            }
-            else if (split[e].includes('caps') & !split[e].includes('national')){
-                let games = split[e+2].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');;
-                gamesArray.push(games);
-                this.state.playerGames = gamesArray;
-            }
-        }
         if (!isLoaded) {
             return (
                 <p>Loading...</p>
