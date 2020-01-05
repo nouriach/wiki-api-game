0ecd08e52c01816b304e28844b174ca487d970f7
nouriach <49981579+nouriach@users.noreply.github.com>
2020-01-05 18:27:41 +0000

    Updated FetchData a bunch
    A lot of work on this. A lot of new functions introduced and a lot of new string manipulation, all of the information can now be accessed. More info in the code comments.

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PlayerClubs from './PlayerClubs';
 import PlayerYears from './PlayerYears';
-import PlayerGoals from './PlayerGoals';
 import PlayerGames from './PlayerGames';
-import { tsExportAssignment } from '@babel/types';
-import { EBADMSG } from 'constants';
+import PlayerName from './PlayerName';
+import PlayAgain from './PlayAgain'
 
 class FetchData extends React.Component {
     constructor(props) {
@@ -16,6 +15,12 @@ class FetchData extends React.Component {
             playerYears: [],
             playerGames: [],
             availableClubs: [],
+
+            randName: '',
+            randPlayerClubs: [],
+            randYears: [],
+            randGames: '',
+
             isLoaded: false,
         };
     }    
@@ -37,13 +42,53 @@ class FetchData extends React.Component {
         .catch(err => console.error(err)
         )
     }
-    
-    // A new API Call
-        /**
-          API brings in a list of every team to play in the Premier League.
-          This will be used to cross reference with the original API call to extract
-          every appearance of a club name. 
-        ***/
+
+    removeDataGaps = (playerPool) => {
+        let playerPoolData = playerPool.toString().split('|');
+        // the below arrays will eventually be asignd to the states
+        let playerNameArray = [];
+        let playerYearsArray = [];
+        let playerGamesArray = [];
+
+        let i;
+        for (i=0; i < playerPoolData.length; i++) {
+            if (playerPoolData[i] === "" || playerPoolData[i].includes('sortname')) {
+                // remove empty spaces
+                playerPoolData.splice(i,1);
+                i--;
+            }
+            else if (playerPoolData[i].includes('flagicon')) {
+                // set player appearances
+                let playerGames = playerPoolData[i+5]
+                playerGamesArray.push(playerGames);
+            }
+            else if (playerPoolData[i].includes('align="left"')){
+                // set player name
+                let nameLoop = `${playerPoolData[i+2]} ${playerPoolData[i+3]}`.replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+                // set player career years
+                let yearsLoop = `${playerPoolData[i-3]} - ${playerPoolData[i-2]}`
+                playerNameArray.push(nameLoop);
+                playerYearsArray.push(yearsLoop);
+            }
+        }
+        //remove bad data at the start
+        playerYearsArray.shift();
+        /**** 
+          Below each state array has the same amount of values in it, therefore
+          number '30' will always represent the same player's information.
+          PlayerClubs is still incomplete.
+         ****/ 
+
+        this.setState ({
+            playerPool: playerPoolData,
+            playerName: playerNameArray,
+            playerClubs: [],
+            playerYears: playerYearsArray,
+            playerGames: playerGamesArray,
+        })
+        this.fetchClubList();
+    }
+
     fetchClubList = () => {
         fetch('https://en.wikipedia.org/w/api.php?&origin=*&action=parse&page=List%20of%20Premier%20League%20clubs&format=json&prop=wikitext&section=1')
         .then (resp => resp.json())
@@ -52,38 +97,42 @@ class FetchData extends React.Component {
                 this.setState ({
                     availableClubs: clubs,
                 })
-                this.buildClubs(this.state.availableClubs)
+                this.cleanClubs(this.state.availableClubs)
             })
         .catch(err => console.error(err)
         )
     }
 
-    buildClubs = (teams) => {
-        console.log('hello from build club');
+    cleanClubs = (teams) => {
         // New API call turned into an array
         let teamData = teams.toString().split('|');
         let finalTeamsArray = []
-        let item;
-        for (item = 0; item < teamData.length; item++) {
-            if (teamData[item] == "") {
+        let value;
+        for (value = 0; value < teamData.length; value++) {
+            if (teamData[value] === "") {
                 // remove gaps in the array
-                teamData.splice(item,1);
+                teamData.splice(value,1);
             }
             /****
              If a value includes 'F.C.' && is below a certain character length (longest team name)
              then it will be added to the finalTeamsArray.
              ***/ 
-            else if (teamData[item].includes('F.C.') && (teamData[item].length < 33)) {
-                let teamName = teamData[item].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+            else if (teamData[value].includes('F.C.') && (teamData[value].length < 33)) {
+                let teamName = teamData[value].replace(/[[`~!@#$%^&*()=_|+\-=?;:'",<>\{\}\[\]\\\/]/gi, '');
                 finalTeamsArray.push(teamName);
             }
         }
-        this.storeClubs(finalTeamsArray)
+        //remove the incorrect team at the end 
+        finalTeamsArray.pop();
+        this.setState ({
+            availableClubs: finalTeamsArray,
+        });
+        this.storeClubs()
     }
 
-    storeClubs = (possibleTeams) => {
+    storeClubs = () => {
         /*****
-        Clubs are now stored in an array without any gaps and with all special character removed
+        Clubs are now stored in an array without any gaps and with all special characters removed
         This function needs: 
             to update the availableClubs state
             maybe seelct a random player ID number and bring in all of it's data from each state
@@ -91,57 +140,86 @@ class FetchData extends React.Component {
             then extract each club that appears in the availableClubs array
             then updated the state to then be displayed below in JSX
          ****/
-        console.log('hello from possible teams');
-        console.log(possibleTeams);
+        //select a random player
+        let randPlayer = this.state.playerName[Math.floor(Math.random()*this.state.playerName.length)];
+        //find out what position in the array this player's ID is
+        let randId = this.state.playerName.indexOf(randPlayer)
+        let selectedFootballer = this.state.playerName[randId];
+        let footballerOption = this.state.playerPool;
 
-    };
-
-    removeDataGaps = (d) => {
-        let allData = d.toString().split('|');
-        let playerNameArray = [];
-        let playerYearsArray = [];
-        let playerGamesArray = [];
+        let footballerNew = selectedFootballer.toString().split(' ');
 
         let i;
-        for (i=0; i < allData.length; i++) {
-            if (allData[i] === "" || allData[i].includes('sortname')) {
-                // remove empty spaces
-                allData.splice(i,1);
-                i--;
+        for (i = 0; i < footballerOption.length; i++) {
+            if (footballerOption[i].includes(footballerNew[1])) {
+                // console.log('random player', randPlayer);
+                // console.log('random id', randId);
+                // console.log('second name of random player', footballerNew[1]);
+                // console.log('string start', (footballerOption[i]));
+                // console.log('string end', footballerOption[i+15]);
+
+                let sliceStart = footballerOption.indexOf(footballerOption[i]);
+                let sliceEnd = footballerOption.indexOf(footballerOption[i+15]);
+                // IF THE BELOW CONSOLE.LOGS DON'T SHOW CORRECTLY THEN THE PLAYER'S CLUBS CAN'T BE CREATED
+                console.log('index slice start:', sliceStart);
+                console.log('index slice end', sliceEnd);
+                let newString = footballerOption.slice(sliceStart, sliceEnd);
+                this.loopClubs(newString, randId, randPlayer);
             }
-            else if (allData[i].includes('flagicon')) {
-                // set player appearances
-                let playerGames = allData[i+5]
-                playerGamesArray.push(playerGames);
-            }
-            else if (allData[i].includes('align="left"')){
-                // set player name
-                let nameLoop = `${allData[i+2]} ${allData[i+3]}`;
-                // set player career years
-                let yearsLoop = `${allData[i-3]} - ${allData[i-2]}`
-                playerNameArray.push(nameLoop);
-                playerYearsArray.push(yearsLoop);
+            else {
             }
         }
-        /**** 
-          Below each state array has the same amount of values in it, therefore
-          number '30' will always represent the same player's information.
-          PlayerClubs is still incomplete.
-         ****/ 
-        this.setState ({
-            playerName: playerNameArray,
-            playerClubs: [],
-            playerYears: playerYearsArray,
-            playerGames: playerGamesArray,
-        })
+    }
 
-        this.fetchClubList();
+    loopClubs = (playerStr, playerId, playerName) => {
+        console.log('loopClubs 1: this should be reduced string', playerStr);
+        console.log('loopClubs 2: this should be the ID', playerId);
+        console.log('loopClubs 3: this should be the player name', playerName);
+
+        let randomPlayerClubs = [];
+        let playerTeams = this.state.availableClubs;
+        let avClub;
+        let playerValue;
+        for (avClub=0; avClub < playerTeams.length; avClub++) {
+            for (playerValue=0; playerValue < playerStr.length; playerValue++) {
+                if (playerStr[playerValue].includes(playerTeams[avClub])) {
+                    console.log('successful match', playerStr[playerValue]);
+                    randomPlayerClubs.push(playerStr[playerValue]);
+                }
+            }
+        }
+        console.log('loopClubs 4: this should be players clubs', randomPlayerClubs)
+        this.setState ({
+            randPlayerClubs: randomPlayerClubs,
+        })
+        this.setPlayer(playerId, randomPlayerClubs)
+    }
+
+    /******
+     * Function below displays all required info in the console.
+     * Some times it fails to bring through the player's teams (may associated with the 'id' number issue
+     * where the number ISN'T value+15 for some reason)
+     **/
+    setPlayer = (id, clubs) => {
+        // console.log('setPlayer 1 ID:', id)
+        // console.log('setPlayer 2 clubs:', clubs)
+        // console.log('setPlayer 3 name:', this.state.playerName[id]);
+        // console.log('setPlayer 4 years:', this.state.playerYears[id]);
+        // console.log('setPlayer 5 games:', this.state.playerGames[id]);
+        this.setState ({
+            randName: this.state.playerName[id],
+            randPlayerClubs: clubs,
+            randYears: this.state.playerYears[id],
+            randGames: this.state.playerGames[id],
+            isLoaded: true,
+        
+        })
+        console.log(this.state)
     }
 
     render() {
-        let { isLoaded, playerName, playerData, playerClubs, playerYears, playerGoals, playerGames } = this.state;
-
-
+        let { isLoaded, randName, randPlayerClubs, randYears, randGames} = this.state;
+        
         if (!isLoaded) {
             return (
                 <p>Loading...</p>
@@ -149,12 +227,23 @@ class FetchData extends React.Component {
         }
         else {
             return (
+                /*** 
+                 * CURRENTLY THE BELOW COMPONENTS ARE 'MAPPING' THROUGH THE STATES AS IF THERE IS
+                 * AN ARRAY. REMOVE THE MAPPING AND PASS THE OPTION DOWN AS A VALUE.
+                 * PERHAPS IN THE FUTURE EACH OF THE ABOVE 'CODE' CAN HAPPEN INSIDE THE COMPONENT, THEREFORE MAPPING 
+                 * WILL BE REQUIRED.
+                 ***/
                 <>
                     <div id="Results">
-                        <p>Name: <span>{this.state.playerName}</span></p>
-                        <PlayerClubs clubs={this.state.playerClubs}/>
-                        <PlayerYears years={this.state.playerYears} />
-                        <PlayerGames games={this.state.playerGames} />
+                        <PlayerClubs clubs={randPlayerClubs}/>
+                        <PlayerYears years={randYears} />
+                        <PlayerGames games={randGames} />
+                    </div>
+                    <div className="nameResults-container">
+                        <PlayerName name={randName}/>
+                    </div>
+                    <div className='play-container'>
+                        <PlayAgain />
                     </div>
                 </>
             )
