00b731b784e5a976e7b31c8f5daeb5295ba7225d
nouriach <49981579+nouriach@users.noreply.github.com>
2020-01-07 14:47:35 +0000

    PlayerName component updated
    > state introduced to manage: game state; hold the current player name and store the user's guess
    > handleChange and handleSubmit functions added to work work with the new submit button and form
    > a function successfully passed down from parent component (FetchData)

--- src/components/PlayerName.js
+++ src/components/PlayerName.js
@@ -1,15 +1,81 @@
 import React from 'react';
 
-const PlayerName = props => {
+class PlayerName extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            guess: '',
+            playerName: props.name,
+            showAnswer: false,
+    };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit  = this.handleSubmit.bind(this);
+    }
 
-    return (
-        <>
-            <div className="contentContainer">
-                <h3>Answer</h3>
-                <p className="result">{props.name}</p>
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState ({
+            [name]: value,   
+        })
+        console.log(value);
+    }
+
+    handleSubmit = (e) => {
+        console.log('hello from handleSubmit')
+        e.preventDefault();
+        console.log(this.state)
+        let playerGuess = this.state.guess;
+        let answer = this.state.playerName;
+        console.log(playerGuess);
+        console.log(answer) ;
+        if (playerGuess === answer) {
+            console.log('correct answer');
+            this.setState({
+                guess: '',
+                showAnswer: true,
+            })
+            console.log('passed name', this.state)
+        }
+        else {
+            console.log('your answer is wrong');
+            this.setState({
+                guess: '',
+            })
+        }
+
+    }
+
+    render () {
+        let { showAnswer } = this.state;
+
+        if (!showAnswer) {
+
+            return( 
+            <div>
+                <form>
+                    <input type="text" placeholder="Who is it?" name="guess" value={this.state.guess} onChange={this.handleChange} />
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                </form>                 
             </div>
-        </>
-    )
+            )
+        }
+        else {
+            return (
+            <>
+                <div>
+                    <form>
+                        <input type="text" placeholder="Who is it?" name="guess" value={this.state.guess} onChange={this.handleChange} />
+                        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                    </form>                 
+                </div>
+                <div className="contentContainer nameResults-container">
+                    <h3>Answer</h3>
+                    <p className="result">{this.state.playerName}</p>
+                </div>
+            </>
+            )
+        }
+    }
 }
 
 export default PlayerName;
